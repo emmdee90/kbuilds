f8655dd08e0fac8c7a580fb1fb7825bc6477abda
emmdee90 <emmdee90@gmail.com>
2025-03-08 01:17:22 +0000

    Port over old workspaces

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
+    name: 'Arc\'s Registry',
     description: 'The official store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://github.com/emmdee90/kbuilds/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/kbuilds/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
